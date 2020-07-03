abe19d460605ff9d9d43a3e4ad6be3897c31c11f
krishna <bkesari@outlook.com>
2020-07-03 17:51:38 -0400

    web

--- Web App.R
+++ Web App.R
@@ -1,2 +1,17 @@
 library(shiny)
 
+ui <- shinyUI(pageWithSidebar(
+                headerPanel("Text area"),
+                sidebarPanel(
+                    radioButtons("area", "Service area",
+                            c("Armadillo", "Baboon",
+                              "Camel", "Deer", "Elephant"),
+                            selected = "Armadillo")
+                ),
+                mainPanel(
+                    h3("Total Posts"),
+                    HTML("<p>Cumulative <em> totals </em> over time</p>"),
+                    plotOutput("plotDisplay"),
+                    htmlOutput("outputLink")
+                )
+            ))
