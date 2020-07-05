ad69054463e6992c56015ec1d5fc9f6f039b531f
krishna <bkesari@outlook.com>
2020-07-05 19:40:03 -0400

    JS

--- JS App.js
+++ JS App.js
@@ -15,15 +15,16 @@
         var textY = 1;
 
         // defining move function
-        function move() {
+    function move() {
             ctx.clearRect(0,0, c.width, c.height);
             ctx.fillText(text, textX, textY * 5);
 
             if(textY++ < 40){
                 setTimeout(move, 25); //delay between frames
+                }
             }
-        }
 
         move(); //call function
     }
+
 </script>
